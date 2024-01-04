68ae91fe3bd52482db88bf468450b5449ab05ee3
SanaMarwa <75172571+SanaMarwa@users.noreply.github.com>
2024-01-04 22:54:17 +0530

    Add files via upload

--- applications.php
+++ applications.php
@@ -1,22 +1,27 @@
 <?php
 
 
-    $firstName = $_POST['fname'];
-    $lasttName = $_POST['lname'];
-    $email = $_POST['email'];
-    $pnum = $_POST['pnum'];
-    $bday = $_POST['dob'];
-    $program = $_POST['program'];
-    // $toddler = $_POST['toddler'];
-    // $primary = $_POST['primary'];
-    // $daycare = $_POST['daycare'];
-    // $summercamp = $_POST['summercamp'];
+$firstName = $_POST['fname'];
+$lastName = $_POST['lname'];
+$email = $_POST['email'];
+$pnum = $_POST['pnum'];
+$dob = $_POST['dob'];
 
-    $mailheader = "From:" .$firstname."<".$email.">\r\n";
-    $recipient = "sana.marwa12@gmail.com";
-    mail($recipient, $subject, $firstName , $mailheader)
+$program = 'None';
+if (isset($_POST['program']) && is_array($_POST['program']) && count($_POST['program']) > 0) {
+    $program = implode(', ', $_POST['program']);
+}
+
+$body = "First Name: " . $firstName . "\r\n" . "Last Name: " . $lastName . "\r\n" . "Email: " . $email . "\r\n" . "Phone Number: " . $pnum . "\r\n" . "DOB: " . $dob . "\r\n" . "Program(s): " . $program;
+
+$subject = "New Enquiry Application!";
+$mailheader = "From:" . $firstname . "<" . $email . ">\r\n";
+$recipient = "elysianmontessori@gmail.com";
+mail($recipient, $subject, $body, $mailheader)
     or die("Error!");
+// echo "message sent";
 
-    echo "message sent";
+header('Location:/thankyou.html');
+// echo '<script>alert("Enquiry Sent Successfully. Thank You for Contacting us.");</script>';
 
- ?>
+?>
